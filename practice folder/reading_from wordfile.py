6eb7b2858d501b27c43d5513e5493e35a8742a97
xXx <mohammadiinn37@gmail.com>
2024-10-10 12:32:05 +0300

    added a way to read tabels from a word file

--- practice folder/reading_from wordfile.py
+++ practice folder/reading_from wordfile.py
@@ -6,3 +6,8 @@ doc = Document("test1.docx")
 for p in doc.paragraphs:
     if p.style.name.startswith("Heading") or p.style.name == "Title":
         print(p.text)
+
+
+for table in doc.tables:
+    for row in table.rows :
+        print("\\".join(cell.text for cell in row.cells))
