6babb602aed94bfd9892c41051a9a0e3d15816fa
xXx <mohammadiinn37@gmail.com>
2024-10-10 14:03:52 +0300

    the code didnt work because i put ==

--- practice folder/writing_in_word_file.py
+++ practice folder/writing_in_word_file.py
@@ -5,11 +5,11 @@ from docx.shared import Pt
 
 doc = Document()
 
-doc.add_heading("fisrt heading", 0)
+doc.add_heading("first heading", 0)
 
 p = doc.add_paragraph("examples of writing changes")
-p.add_run("\n this text is bold").bold == True
-p.add_run("\n this text is italic").italic == True
+p.add_run("\n this text is bold").bold = True
+p.add_run("\n this text is italic").italic = True
 
 
 
