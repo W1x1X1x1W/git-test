4571f465006aa88df2d52a0be8a56cf2178a9bde
xXx <mohammadiinn37@gmail.com>
2024-10-10 14:00:45 +0300

    some small changes in the text like bold and italic

--- practice folder/writing_in_word_file.py
+++ practice folder/writing_in_word_file.py
@@ -1,9 +1,17 @@
 from docx import Document
+from docx.shared import Pt
 
 
 
 doc = Document()
 
 doc.add_heading("fisrt heading", 0)
+
+p = doc.add_paragraph("examples of writing changes")
+p.add_run("\n this text is bold").bold == True
+p.add_run("\n this text is italic").italic == True
+
+
+
 
 doc.save("writing.docx")
