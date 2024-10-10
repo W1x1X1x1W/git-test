edfe9a563a559943e05178758f91e927c51c7fa8
xXx <mohammadiinn37@gmail.com>
2024-10-10 15:27:31 +0300

    improve the paragraph change code

--- practice folder/writing_in_word_file.py
+++ practice folder/writing_in_word_file.py
@@ -17,16 +17,22 @@ doc.add_paragraph("create a bullet 1 \n", style="List Bullet")
 doc.add_paragraph("create a bullet 2 \n", style="List Bullet")
 
 '''add paraghraph with diifernt font and size for every paragraph'''
-
+doc.add_paragraph("this is the first paragraph \n")
 style = doc.styles["Normal"]
 font = style.font
 font.name = "MS Gothic"
 font.size = Pt(20)
 
 
-paragraph1 = doc.add_paragraph("..............")
-p1 = paragraph1.add_run("this is the seconde paragraph!!!")
-p1.font.size = Pt(72)
+paragraph1 = doc.add_paragraph("..............11\n")
+p1 = paragraph1.add_run("this is the second paragraph!!! \n")
+p1.font.size = Pt(48)
+p1.font.name = "Arial"
+
+paragraph2 = doc.add_paragraph("............22 \n")
+p2 = paragraph2.add_run("this is the third paragraph!!!")
+p2.font.size = Pt(72)
+p2.font.name = "MS Gothic"
 
 
 
