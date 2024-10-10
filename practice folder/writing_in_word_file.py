3a385c91d339a9334fdf32763839c8b0a406c8c8
xXx <mohammadiinn37@gmail.com>
2024-10-10 15:17:12 +0300

    adding two pragraph with differnt sizes

--- practice folder/writing_in_word_file.py
+++ practice folder/writing_in_word_file.py
@@ -4,9 +4,11 @@ from docx.shared import Pt
 
 
 doc = Document()
+'''add heading text'''
 
 doc.add_heading("first heading", 0)
 
+'''add paragraphs'''
 p = doc.add_paragraph("examples of writing changes")
 p.add_run("\n this text is bold").bold = True
 p.add_run("\n this text is italic").italic = True
@@ -14,6 +16,17 @@ p.add_run("\n this text is italic").italic = True
 doc.add_paragraph("create a bullet 1 \n", style="List Bullet")
 doc.add_paragraph("create a bullet 2 \n", style="List Bullet")
 
+'''add paraghraph with diifernt font and size for every paragraph'''
+
+style = doc.styles["Normal"]
+font = style.font
+font.name = "MS Gothic"
+font.size = Pt(20)
+
+
+paragraph1 = doc.add_paragraph("..............")
+p1 = paragraph1.add_run("this is the seconde paragraph!!!")
+p1.font.size = Pt(72)
 
 
 
