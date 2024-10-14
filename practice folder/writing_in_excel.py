c0d3c54bc1094d14d334ef17de985230bcd03b42
xXx <mohammadiinn37@gmail.com>
2024-10-14 12:30:09 +0300

    getting the locations of cells

--- practice folder/writing_in_excel.py
+++ practice folder/writing_in_excel.py
@@ -1,9 +1,14 @@
 from openpyxl import Workbook, load_workbook
-wb = Workbook()
+from openpyxl.utils import get_column_letter
+wb = load_workbook('excelFile_new.xlsx')
 ws = wb.active
 ws.title = 'sh1'
-list1 = ['name','job','age']
-ws.append(list1)
+
+for row in range(1,7):
+    for col in range(1,4):
+        char = get_column_letter(col)
+        print(char + str(row))
+
 
 
 
