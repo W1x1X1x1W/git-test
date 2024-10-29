0af62458794c5d793b399e79f345335dffd4742e
xXx <mohammadiinn37@gmail.com>
2024-10-29 11:19:23 +0200

    new messege

--- chat_gpt/text_complation.py
+++ chat_gpt/text_complation.py
@@ -10,9 +10,10 @@ client = OpenAI()
 
 response = client.chat.completions.create(
   model="gpt-4o",
+  response_format={ "type": "json_object" },
   messages=[
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": "just say 'hey' "},
+    {"role": "system", "content": "be stric data anylisize person as pasable"},
+    {"role": "user", "content": "genrate for me the names of all week days and put for every day a one word discribing the weather i want evrething i json format and put them all in a dictionry called 'happy days' "},
   ]
 )
 
